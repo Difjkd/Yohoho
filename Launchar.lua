451bafc59cd141c8663390cee891991ecc4328b3
Difjkd <alphahentai18@gmail.com>
2024-12-06 11:51:56 +0700

    Launchar.lua

---
+++ Launchar.lua
@@ -0,0 +1,22 @@
+repeat task.wait(0.25) until game:IsLoaded();
+getgenv().CurrentVersion = "1.13";
+
+--------------------------------[ Main Loading UI ]------------------------------------
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lvl9999/Loader/main/UI"))();
+---------------------------------------------------------------------------------------
+
+local SupportedGames = {
+    A_Universal_Time = {6846458508,5130598377,8023712967};
+    Heaven_Stand = {14561998168};
+    Sakura_Stand = {8534845015};
+}
+
+if table.find(SupportedGames.A_Universal_Time,game.PlaceId) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Difjkd/My-Wife-FlameW-/refs/heads/main/AUT?token=GHSAT0AAAAAACYXC5I4OZRBLYEIGBUICXOKZYTMMCQ"))();
+elseif table.find(SupportedGames.Heaven_Stand,game.PlaceId) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Difjkd/FlamesW/refs/heads/new/HS"))();
+elseif table.find(SupportedGames.Sakura_Stand,game.PlaceId) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Difjkd/My-Wife-FlameW-/refs/heads/main/SS?token=GHSAT0AAAAAACYXC5I4TWURMM6U4GQG4TFQZYTMNAA"))();
+else
+    getgenv().Notify("http://www.roblox.com/asset/?id=11803247078","Flame's Notification","Bro really tried to use my script on an unsupported game  👏","","",9e9);
+end
